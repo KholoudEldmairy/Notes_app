3b6219c2e405f5db91f667b2af89fb65901280c6
Kholoud <khlwdaldmyrya@gmail.com>
2026-01-28 07:30:08 +0200

    add add note state

--- lib/Cubits/cubit/add_new_cubit_state.dart
+++ lib/Cubits/cubit/add_new_cubit_state.dart
@@ -4,3 +4,15 @@ part of 'add_new_cubit_cubit.dart';
 sealed class AddNewCubitState {}
 
 final class AddNewCubitInitial extends AddNewCubitState {}
+
+final class AddNewCubitLoading extends AddNewCubitState {}
+
+final class AddNewCubitSuccess extends AddNewCubitState {}
+
+final class AddNewCubitFailure extends AddNewCubitState {
+  final String errMessage;
+
+  AddNewCubitFailure( this.errMessage);
+
+
+}

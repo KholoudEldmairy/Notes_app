511b4047729bca107263811c24a8b0b214ea78ea
Kholoud <khlwdaldmyrya@gmail.com>
2026-01-28 07:39:34 +0200

    register adaptor

--- lib/Cubits/cubit/add_new_cubit_cubit.dart
+++ lib/Cubits/cubit/add_new_cubit_cubit.dart
@@ -1,8 +1,16 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/models/noteModel.dart';
 
 part 'add_new_cubit_state.dart';
 
 class AddNewCubitCubit extends Cubit<AddNewCubitState> {
   AddNewCubitCubit() : super(AddNewCubitInitial());
+
+
+  addNote(NoteModel Note){
+
+    
+
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:notes_app/models/noteModel.dart';
 import 'package:notes_app/views/homeView.dart';
 import 'package:notes_app/widgets/constants.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox(kNoteBox);
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
 }
 
